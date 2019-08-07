dce1bf9587ca5648721ccfa40adb0069f89a5199
zgf <625159342@qq.com>
2019-08-07 18:18:24 +0800

    1.修复引用问题

--- app/src/main/java/com/biansemao/stackcardlayoutmanager/MainActivity.kt
+++ app/src/main/java/com/biansemao/stackcardlayoutmanager/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import com.thai.thishop.weight.layoutmanager.StackCardLayoutManager
 
 class MainActivity : AppCompatActivity() {
 
